9c91dec0e7dbfea479b7581248bf1b27ab417767
Dmitrii Chudinov <dima@localhost.localdomain>
2020-05-16 12:30:32 +0300

    Add data in TK

--- test.py
+++ test.py
@@ -5,18 +5,20 @@ lib = cdll.LoadLibrary("./chudinov.so")
 lib.IsSimple.argtypes = [c_int]
 lib.IsSimple.restype = c_bool
 
-simpleInt = c_int(2)
-print(lib.IsSimple(simpleInt))
-
-simpleInt = c_int(4)
-print(lib.IsSimple(simpleInt))
-
 lib.NextSimple.argtypes = [c_int]
 lib.NextSimple.restype = c_int
 
-simpleInt = c_int(2)
+simpleInt = c_int(11)
+print("For integer 11")
+print("Is simple: ")
+print(lib.IsSimple(simpleInt))
+print("Next simple: ")
 print(lib.NextSimple(simpleInt))
 
-simpleInt = c_int(7)
+simpleInt = c_int(14)
+print("For integer 14")
+print("Is simple: ")
+print(lib.IsSimple(simpleInt))
+print("Next simple: ")
 print(lib.NextSimple(simpleInt))
 
